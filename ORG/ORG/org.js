aa39cedbffc3a4d7acbb9d63dffdd5553956fb68
nasir hanif <nasir127sb@gmail.com>
2023-06-14 16:20:56 +0500

    pankeg website

---
+++ ORG/ORG/org.js
@@ -0,0 +1,26 @@
+
+
+document.getElementById(mybtn).addEventListener("click",function(){
+
+   var mybox = document.getElementById("rto");
+
+if(mybox.style.display=="none"){
+mybox.style.display="block";
+
+}
+else{
+    mybox.style.display="none";
+}
+
+});
+
+
+
+
+
+
+
+
+
+
+
